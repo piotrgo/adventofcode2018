e3fb6c8a032633926306ff7c1d545824e583632a
Piotr Gospodarek <p.gospodarek@kainos.com>
2018-12-05 23:56:43 +0100

    Day 2, part 2

--- 02/02.py
+++ 02/02.py
@@ -1,8 +1,10 @@
 from collections import Counter
+from difflib import SequenceMatcher
 
 twos = 0
 threes = 0
-
+difference = []
+best_pair = []
 
 with open('input_data') as f:
     lines = f.read().splitlines()
@@ -18,5 +20,23 @@ for line in lines:
     if a in [9, 3]:
         threes +=1
 
+#for part one
 result = twos*threes
 print(result)
+
+# part two:
+
+def similar(a, b):
+    return SequenceMatcher(None, a, b).ratio()
+
+
+i = 0
+while i < len(lines):
+    j = 0
+    while j < len(lines):
+        difference.append([similar(lines[i], lines[j]), i, j])
+        j += 1
+    i += 1
+
+best_pair = sorted(difference)[:-(len(lines))][len(best_pair)-2:-1]
+print(''.join([i for i, j in zip(lines[best_pair[0][1]], lines[best_pair[0][2]]) if i == j]))
