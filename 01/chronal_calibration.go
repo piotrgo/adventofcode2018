c17124bd3b6c9ae196fc31aacef753047772e758
Piotr Gospodarek <p.gospodarek@kainos.com>
2018-12-05 22:09:55 +0100

    Day 1, part 0. Last attempt at solving with Go

--- 01/chronal_calibration.go
+++ 01/chronal_calibration.go
@@ -7,21 +7,27 @@ import (
 	"strconv"
 )
 
+var numbers []int
+var sum []int
+var sumA int
+var result int
+
 func check(e error) {
 	if e != nil {
 		panic(e)
 	}
 }
 
-func frequency(path string) int {
-	var numbers []int
-	var sum int
+func parseInput(path string) []int {
+
 	file, err := os.Open(path)
 	check(err)
 	defer file.Close()
+	lineCount := 0
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
+		lineCount++
 		value, err := strconv.Atoi(scanner.Text())
 		check(err)
 		numbers = append(numbers, value)
@@ -29,12 +35,51 @@ func frequency(path string) int {
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "reading standard input:", err)
 	}
-	for _, num := range numbers {
-		sum += num
+
+	return numbers
+}
+
+func frequencySteps(something []int) []int {
+	for i := 2; i <= len(something); i++ {
+		sumA = 0
+		for _, num := range something[0:i] {
+			sumA += num
+		}
+		sum = append(sum, sumA)
 	}
+
 	return sum
 }
 
+func tunedFrequency(elements []int) int {
+	// Use map to record duplicates as we find them.
+	encountered := map[int]bool{}
+	//result := 0
+	var result int
+
+	for v := range elements {
+		fmt.Println(elements[v])
+		if encountered[elements[v]] == true {
+			result = elements[v]
+		} else {
+			// Record this element as an encountered element.
+			encountered[elements[v]] = true
+		}
+	}
+	return result
+}
+
 func main() {
-	fmt.Println(frequency(os.Args[1]))
+	inputData := parseInput(os.Args[1])
+	var s3 []int
+	// increase the counter if not found within this limit :P
+	for i := 0; i < 10; i++ {
+		s1 := frequencySteps(inputData)
+		s2 := tunedFrequency(s1)
+		// if s2 != 0 {
+		fmt.Println(s2)
+		// }
+		s3 = []int{s1[len(s1)-1]}
+		inputData = append(s3, parseInput(os.Args[1])...)
+	}
 }
