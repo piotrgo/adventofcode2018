16f925292f095e9f7a3b431ff1a81dd7aadd0c82
Piotr Gospodarek <piogospo@gmail.com>
2019-10-02 11:40:44 +0200

    Added comments to the code.

--- 04/04.py
+++ 04/04.py
@@ -10,9 +10,11 @@ with open('input') as f:
 def part1(lines):
     pattern = {}
     totals = {}
+    # define regexes to fetch data from input
     guard_r = re.compile("Guard #(\d+)")
     asleep_r = re.compile(".+:(\d\d).*falls.*$")
     wakeup_r = re.compile(".+:(\d\d).*wakes.*$")
+    # parse input
     for entry in lines:
         if guard_r.search(entry):
             guardID = guard_r.findall(entry)[0]
@@ -20,6 +22,7 @@ def part1(lines):
             sleep = int(asleep_r.findall(entry)[0])
         elif wakeup_r.search(entry):
             wake = int(wakeup_r.findall(entry)[0])
+            # populate dictionary based on guard id
             for minute in range(sleep, wake):
                 num = 1
                 if guardID not in pattern:
@@ -28,16 +31,21 @@ def part1(lines):
                     num = pattern[guardID][minute]
                     num += 1
                 pattern[guardID][minute] = num
+    # calculate total sleep time for each guard
     for key in pattern.keys():
         totals[key] = sum(pattern[key].values())
+    # find a guard who sleeps the most
     sleeper = (max(totals, key=totals.get))
+    # find the most slept minute for that guard (sleeper)
     minutes = dict(pattern[sleeper].items())
     most_slept_minute=(max(minutes, key=minutes.get))
+    # result for part 1
     target = int(sleeper)*int(most_slept_minute)
     print("Target =", target)
 
 
 def part2(lines):
+    # the same pattern for parsing input, actually this could be moved to it's own function
     pattern = {}
     totals = {}
     guard_r = re.compile("Guard #(\d+)")
@@ -62,14 +70,17 @@ def part2(lines):
     h_dur = 0
     h_min = 0
     h_id = 0
+    # find most slept minute, and it's duration as well as id
     for gid, value in pattern.items():
         most_slep_minute = (max(value, key=value.get))
         sleep_duration = (value.get(max(value, key=value.get)))
         reg.append([gid, most_slep_minute, sleep_duration])
+        # while parsing list find the biggest value and record the rest of metadata
         if sleep_duration > h_dur:
             h_dur = sleep_duration
             h_min = most_slep_minute
             h_id = gid
+    # result for part 2
     print("Guard id:",h_id,"minute:", h_min, "result:", int(h_id)*int(h_min))
 
 part1(lines)
