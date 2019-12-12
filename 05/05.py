6177cf2fbea3b43bd2bcc824b0a7eac5451717a9
Piotr Gospodarek <p.gospodarek@kainos.com>
2019-12-12 23:39:42 +0100

    Day 5, part 2. Probably the slowest, not on purpose, solution out there

--- 05/05.py
+++ 05/05.py
@@ -5,6 +5,8 @@ with open('input') as f:
 polymer_list = list(polymer)
 size = len(polymer_list)
 new_size = len(polymer_list) - 1
+unit_types = []
+results = []
 
 
 def react(input_list):
@@ -26,5 +28,32 @@ while new_size < size:
     react(polymer_list)
     new_size = len(polymer_list)
 
-# solution
+# solution part 1
 print(len(polymer_list))
+
+
+# get the list of unit types to evaluate
+def char_range(c1, c2):
+    """Generates the characters from `c1` to `c2`, inclusive."""
+    for c in range(ord(c1), ord(c2)+1):
+        yield chr(c)
+
+
+for c in char_range('a', 'z'):
+    unit_types.append(c)
+
+for u in unit_types:
+    print(u)
+    polymer_list = list(polymer)
+    polymer_list = [x for x in polymer_list if x != u]
+    polymer_list = [x for x in polymer_list if x != u.upper()]
+    size = len(polymer_list)
+    new_size = len(polymer_list) - 1
+    while new_size < size:
+        size = new_size
+        react(polymer_list)
+        new_size = len(polymer_list)
+    results.append(new_size)
+
+# solution part 2
+print(min(results))
